89e0d9f859c1fff34bd53d854dcce12a28f18b82
Hwiseong <hw969655@dal.ca>
2024-09-16 16:32:28 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-
+import React from "react";
+import Navbar from "./Components/Navbar";
 function App() {
   return (
     <div className="App">
+
+      <>
+      
+      </>
       <h1>Yo</h1>
     </div>
   );
