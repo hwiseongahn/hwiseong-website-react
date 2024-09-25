363aacec2bcf5130436977ec022cc74642f9561c
Hwiseong <hw969655@dal.ca>
2024-09-25 17:23:46 -0300

    made navbar hoverable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+import Projects from "./Components/Projects";
 import { Route, Routes } from "react-router-dom";
+import "./App.scss";
 
 function Home() {
         return (
             <>
                 <div className="Hero">
                     <h1>HI</h1>
+                    <h2>is this the body</h2>
                 </div>
             </>
         );
@@ -16,14 +20,14 @@ function Home() {
 function App() {
     return (
         <>
-
                 <Navbar/>
                 <div className="container">
                 <Routes>
                     <Route path= "/" element={<Home/>}/>
+                    <Route path ="/About" element={<About/>}/>
+                    <Route path= "/Projects" element={<Projects/>}/>
                 </Routes>
                 </div>
-
         </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,77 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import { motion } from "framer-motion";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faSquareGithub } from "@fortawesome/free-brands-svg-icons";
 import "../scss/navbar.scss";
-function Navbar() {
+
+const Navbar = () => {
+
+    const ScreenWidthCheck = () => {
+        const [isDesktop, setDesktop] = useState(window.innerWidth > 650);
+
+        const updateMedia = () => {
+            setDesktop(window.innerWidth >= 650);
+        };
+
+        useEffect(() => {
+            window.addEventListener("resize", updateMedia);
+            return () => window.removeEventListener("resize", updateMedia);
+        }, []);
+
+        return (
+            <>
+                {isDesktop ? (
+                    <NavbarDesktop />
+                ) : (
+                    <NavbarMobile />
+                )}
+            </>
+        );
+    };
+
+    return <ScreenWidthCheck />;
+};
+
+const NavbarMobile = () => {
     return (
-        <>
+        <div>
+            THIS MEANS THE NAVBAR IS MOBILE!!!
+        </div>
+    );
+};
+
+const NavbarDesktop = () => {
+    return (
         <nav className="navbar">
-            
-            <div className="nav-links">
                 <div className="navbar-left">
-                    <Link className="link" to="/"> 
-                        Hwiseong Ahn
-                    </Link>
-                    <Link className="link" to="/About">
-                        About
-                    </Link>
-                    <Link className="link" to="/Projects">
-                        Projects
-                    </Link>
+                    <Link className="logo" to="/">MY NAME</Link>
+                    <CustomLink to="/About">ABOUT</CustomLink>
+                    <CustomLink to="/Projects">PROJECTS</CustomLink>
                 </div>
                 <div className="navbar-right">
-                        <motion.a href="https://www.linkedin.com/in/hwiseongahn/">
+                    <motion.a className="link" href="https://www.linkedin.com/in/hwiseongahn/">
+                        LINKED
                         <FontAwesomeIcon className="icon" icon={faLinkedin} />
-                        </motion.a>
-                        <motion.a href="https://www.linkedin.com/in/hwiseongahn/">
+                    </motion.a>
+                    <motion.a className="link" href="https://github.com/hwiseongahn">
+                        GITHUB
                         <FontAwesomeIcon className="icon" icon={faSquareGithub} />
-                        </motion.a>
+                    </motion.a>
                 </div>
-            </div>
         </nav>
-        </>
-    )
-}
+    );
+};
 
-export default Navbar
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+  
+    return (
+        <Link className={isActive ? "active-link" : "link"} to={to} {...props}>
+          {children}
+        </Link>
+    )
+  }
+
+export default Navbar;
