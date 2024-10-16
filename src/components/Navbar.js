c88d4ec088f08c765fe7f0cf85339123a3384d6a
Hwiseong <hw969655@dal.ca>
2024-10-16 14:23:01 -0300

    yay mobile navbar looking good

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const MobileNavLinks = ({open, setOpen }) => {
     return (
         <>
         <div className="link-container">
-        <Link className="link" to="/" onClick={onLinkClick}>
+            <Link className="link" to="/" onClick={onLinkClick}>
                 Home
             </Link>
             <Link className="link" to="/About" onClick={onLinkClick}>
@@ -50,6 +50,52 @@ const MobileNavLinks = ({open, setOpen }) => {
             </Link>
             <Link className="link" to="/Projects" onClick={onLinkClick}>
                 Projects
+            </Link>
+            <Link className="link" to="https://www.linkedin.com/in/hwiseongahn/">
+                <motion.p
+                    className="linked-text"
+                    variants={slideUpAnimation}
+                    initial="initial"
+                    whileInView={"animate"}
+                    transition={{delay:0.5}}
+                >
+                    Linked
+                </motion.p>
+                <motion.div
+                    variants={springAnimation}
+                    initial="initial"
+                    whileInView={"animate"}
+                    transition={{
+                        delay: 0.75,
+                        type: "spring",
+                        stiffness: 260,
+                        damping: 20
+                    }}>
+                        <FontAwesomeIcon className="icon" icon={faLinkedin} />
+                </motion.div>
+            </Link>
+            <Link className="link" to="https://github.com/hwiseongahn">
+                <motion.p
+                    className="linked-text"
+                    variants={slideUpAnimation}
+                    initial="initial"
+                    whileInView={"animate"}
+                    transition={{delay:0.5}}
+                >
+                    Github
+                </motion.p>
+                <motion.div
+                    variants={springAnimation}
+                    initial="initial"
+                    whileInView={"animate"}
+                    transition={{
+                        delay: 0.75,
+                        type: "spring",
+                        stiffness: 260,
+                        damping: 20
+                    }}>
+                        <FontAwesomeIcon className="icon" icon={faSquareGithub} />
+                </motion.div>
             </Link>
         </div>
             
@@ -104,7 +150,7 @@ const NavbarDesktop = () => {
     return (
         <nav className="navbar">
                 <div className="navbar-left">
-                    <Link className="logo" to="/">HWISEONG AHN</Link>
+                    <CustomLink className="logo" to="/">HWISEONG AHN</CustomLink>
                     <CustomLink to="/About">ABOUT</CustomLink>
                     <CustomLink to="/Projects">PROJECTS</CustomLink>
                 </div>
@@ -115,9 +161,7 @@ const NavbarDesktop = () => {
                             variants={slideUpAnimation}
                             initial="initial"
                             whileInView={pageLoaded ? "animate" : "initial"}
-                            transition={{
-                                delay:0.75,
-                            }}
+                            transition={{delay:0.75}}
                         >
                             LINKED
                         </motion.p>
@@ -140,9 +184,7 @@ const NavbarDesktop = () => {
                             variants={slideUpAnimation}
                             initial="initial"
                             whileInView={pageLoaded ? "animate" : "initial"}
-                            transition={{
-                                delay:0.75,
-                            }}
+                            transition={{delay:0.75}}
                         >
                             GITHUB
                         </motion.p>
