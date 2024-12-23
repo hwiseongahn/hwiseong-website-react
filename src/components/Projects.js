8ae6e633f33011e4c0644cfe995f2de36f038803
Hwiseong <hw969655@dal.ca>
2024-12-23 18:20:09 -0400

    basically done the phone

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "../scss/projects.scss";
 import Card from "../components/Card"
 import Discord from "../assets/discord.png"
+import Logo from "../assets/favicon.png"
 
 function Projects() {
   return (
@@ -9,7 +10,7 @@ function Projects() {
         <h1>Projects</h1>
         <p className="projects-text">These are some projects I've created.</p>
         <Card title="summBot" description="This Discord bot summarizes conversations. Software used: Python, discord.py, and Gemini API for text analysis." image={Discord} link="https://discord.com/oauth2/authorize?client_id=1293394343297679390" linkDescription='Link'/>
-        <Card title="Portfolio Website" description="This Discord bot summarizes conversations. Software used: Python, discord.py, and Gemini API for text analysis." image={Discord} link="https://discord.com/oauth2/authorize?client_id=1293394343297679390" linkDescription='Link'/>
+        <Card title="Portfolio Website" description="Portfolio website. Software used: React.js, SCSS" image={Logo}/>
     </div>
   )
 }
