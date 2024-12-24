0cb2f44ab4114efb99d651be61cfac3c83d8aff8
Hwiseong <hw969655@dal.ca>
2024-12-24 00:20:08 -0400

    fixed aboutme

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,4 +4,4 @@ export const aboutMe = "Hi I’m Hwiseong! Welcome to my website. " +
 "I’m in my second year of studying Computer Science at Dalhousie University. " + 
 "While I’m exploring different aspects of software development, I find front-end development to be the most enjoyable. " +
 "Besides programming, I work as a Korean/English interpreter locally in Nova Scotia, assisting at provincial courts and hospitals. " +
-"Daesang Corporation and Acadian Seaplants during their business meetings here in Halifax.";
+"I've also had the opportunity to work with Daesang Corporation and Acadian Seaplants during their business meetings here in Halifax.";
