31ca42e9df4e98654da3bf3a7bc521026f565fb9
Hwiseong <hw969655@dal.ca>
2024-12-23 23:23:48 -0400

    changed aboutMe style

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,7 @@
 export { ReactComponent as MoonIcon } from './moon.svg';
 export { ReactComponent as SunIcon } from './sun.svg';
 export const aboutMe = "Hi I’m Hwiseong! Welcome to my website. " +
-"I’m in my second year studying Computer Science at Dalhousie University. " + 
-"While I’m exploring different aspects of software development, I find front-end development the most enjoyable. " +
-"Besides programming, I’m a Korean/English Interpreter working locally in Nova Scotia at provincial courts " +
-"and hospitals. I’ve also had the opportunity to work as an interpreter with " +
+"I’m in my second year of studying Computer Science at Dalhousie University. " + 
+"While I’m exploring different aspects of software development, I find front-end development to be the most enjoyable. " +
+"Besides programming, I work as a Korean/English interpreter locally in Nova Scotia, assisting at provincial courts and hospitals. " +
 "Daesang Corporation and Acadian Seaplants during their business meetings here in Halifax.";
